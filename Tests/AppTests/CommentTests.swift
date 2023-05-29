af8d4aeda7dca508f1751f83832ca041be912ef5
Kryvodub Oleksandr <krivodubag@gmail.com>
2023-05-29 21:59:41 +0300

    Comment tests

---
+++ Tests/AppTests/CommentTests.swift
@@ -0,0 +1,51 @@
+@testable import App
+import XCTVapor
+
+final class CommentTests: XCTestCase {
+    let commentsURI = "/api/comments/"
+    let commentName = "Comment 1"
+    let commentMessage = "Test message 1"
+    var app: Application!
+    
+    override func setUp() {
+        app = try! Application.testable()
+    }
+    
+    override func tearDown() {
+        app.shutdown()
+    }
+    
+    func testCommentsCanBeRetrievedFromAPI() throws {
+        let comment = try Comment.create(name: commentName, message: commentMessage, on: app.db)
+        _ = try Comment.create(on: app.db)
+
+        try app.test(.GET, commentsURI, afterResponse: { response in
+            let comments = try response.content.decode([App.Comment].self)
+            XCTAssertEqual(comments.count, 2)
+            XCTAssertEqual(comments[0].name, commentName)
+            XCTAssertEqual(comments[0].message, commentMessage)
+            XCTAssertEqual(comments[0].id, comment.id)
+        })
+    }
+    
+    func testCommentCanBeSavedWithAPI() throws {
+        let comment = Comment(name: commentName, message: commentMessage, postID: .init())
+
+        try app.test(.POST, commentsURI, loggedInRequest: true, beforeRequest: { request in
+            try request.content.encode(comment)
+        }, afterResponse: { response in
+            let receivedComment = try response.content.decode(Comment.self)
+            XCTAssertEqual(receivedComment.name, commentName)
+            XCTAssertEqual(receivedComment.message, commentMessage)
+            XCTAssertNotNil(receivedComment.id)
+
+            try app.test(.GET, commentsURI, afterResponse: { response in
+                let comments = try response.content.decode([App.Comment].self)
+                XCTAssertEqual(comments.count, 1)
+                XCTAssertEqual(comments[0].name, commentName)
+                XCTAssertEqual(comments[0].message, commentMessage)
+                XCTAssertEqual(comments[0].id, receivedComment.id)
+            })
+        })
+    }
+}
